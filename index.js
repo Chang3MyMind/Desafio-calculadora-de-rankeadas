747478ace43012d1d9a4b77f867dfa72a9e2089c
Matheus Mattos <matheusmattos.r@gmail.com>
2024-06-21 09:56:10 -0300

    Colocando Functions para fazer o calculo de elo do Heroi

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
 const prompt = require("prompt-sync")()
+
+calcularElo()
+
+
+
+
+
+function pegarVitorias() {
+    vitoriasDoJogador = vitorias = prompt("Quantas vitoras você teve jogador? ")
+}
+
+function pegarDerrotas(){
+    derrotasDoJogador = derrotas = prompt("Quantas derrotas você teve jogador?")
+}
+
+function calcularElo() {
+    const frase = "O Herói tem de saldo de "
+    const frase2 = " está no nível de "
+    pegarVitorias()
+    pegarDerrotas()
+    let saldoRankeadas = vitorias - derrotas
+
+    if(saldoRankeadas <= 10){
+        console.log(frase + saldoRankeadas + frase2 + "Ferro")
+    }else if (saldoRankeadas >= 11 && saldoRankeadas <= 20){
+        console.log(frase + saldoRankeadas + frase2 + "Bronze")
+    }else if (saldoRankeadas >= 21 && saldoRankeadas <= 50){
+        console.log(frase + saldoRankeadas + frase2 + "Prata")
+    }else if (saldoRankeadas >= 51 && saldoRankeadas <= 80){
+        console.log(frase + saldoRankeadas + frase2 + "Ouro")
+    }else if (saldoRankeadas >= 81 && saldoRankeadas <= 90){
+        console.log(frase + saldoRankeadas + frase2 + "Diamante")
+    }else if (saldoRankeadas >= 91 && saldoRankeadas <= 100){
+        console.log(frase + saldoRankeadas + frase2 + "Lendário")
+    }else if (saldoRankeadas >= 101) {
+        console.log(frase + saldoRankeadas + frase2 + "Imortal")
+    }
+}
